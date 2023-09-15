f6a359b2a1b99483388075a696acca71ca309f17
Brett MacIsaac <brett.macisaac@outlook.com>
2023-09-15 14:27:54 +1000

    Made a minor change to the service-worker registration, which may remove the bug whereby the app would get the new assets but not load them.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
-//import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
@@ -9,3 +9,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 //   </React.StrictMode>
     <App />
 );
+
+/*
+* Configure automatic page reload.
+* Without this, the app will use the old code/assets, rather than reloading the app with the newly received data from 
+  the server.
+* See documentation for more details: https://vite-pwa-org.netlify.app/guide/auto-update.html.
+*/
+if ("serviceWorker" in navigator) 
+{
+    registerSW({ immediate: true });
+}
